8b3135db53b57b37363feabd0091a7c5c3cfc5e5
mysoria <150163606+mysoriaa@users.noreply.github.com>
2024-01-31 12:00:18 +0100

    FeatAdresseCrud

---
+++ src/Form/AdresseType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Adresse;
+use App\Entity\Pays;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdresseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('ville_adr')
+            ->add('cp_adr')
+            ->add('complement_adr')
+            ->add('rue_adr')
+            ->add('pays', EntityType::class, [
+                'class' => Pays::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Adresse::class,
+        ]);
+    }
+}
